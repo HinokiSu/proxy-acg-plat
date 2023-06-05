06c4fb0450e923684856dc163e81227f0051cee3
HinokiSu <yukihinoki@qq.com>
2023-06-05 17:32:13 +0800

    feat: add empty component

---
+++ web/src/components/empty/empty.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="empty">
+    {{ tips }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Empty',
+  props: {
+    tips: {
+      type: String,
+      default: 'Empty'
+    }
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.empty {
+    margin: auto;
+}
+</style>
