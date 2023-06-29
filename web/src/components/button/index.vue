d251d4d8dc70742d4b7e2aad49022b0c6725a78b
HinokiSu <yukihinoki@qq.com>
2023-06-29 17:19:10 +0800

    feat: all new button component

--- web/src/components/button/index.vue
+++ web/src/components/button/index.vue
@@ -4,19 +4,33 @@
     :disabled="isDisabled"
     @click="clickHandler"
     :class="classes"
+    :style="styles"
   >
-    <div class="button-prefix-icon" v-if="hasPreIconSlot">
+    <div
+      class="button-prefix-icon"
+      v-if="hasPreIconSlot"
+      :class="isOnlyIcon ? 'only' : ''"
+    >
       <slot name="preIcon"></slot>
     </div>
-    <div class="acg-button-text">
+    <span v-if="!isOnlyIcon" class="acg-button__text">
       {{ title }}
-    </div>
+    </span>
   </button>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import inlineStyles from '@hooks/inline-styles'
+import { PropType, computed, defineComponent } from 'vue'
 
+type TColor =
+  | 'default'
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'warning'
+  | 'error'
+  | 'gradient'
 export default defineComponent({
   name: 'AcgButton',
   props: {
@@ -25,8 +39,16 @@ export default defineComponent({
       default: ''
     },
     color: {
-      type: String,
+      type: String as PropType<TColor>,
       default: 'default'
+    },
+    w: {
+      type: String,
+      default: 'auto'
+    },
+    h: {
+      type: String,
+      default: '2.5rem'
     },
     disabled: {
       type: Boolean,
@@ -35,6 +57,14 @@ export default defineComponent({
     active: {
       type: Boolean,
       default: false
+    },
+    flat: {
+      type: Boolean,
+      default: false
+    },
+    fillWd: {
+      type: String,
+      default: '0.875rem'
     }
   },
   emits: ['click'],
@@ -54,13 +84,29 @@ export default defineComponent({
       if (propsRef.value.active) {
         _a.push('is-active')
       }
+      if (propsRef.value.flat) {
+        _a.push('flat')
+      }
       return _a
+    })
+
+    const styles = inlineStyles({
+      width: propsRef.value.w,
+      height: propsRef.value.h,
+      'padding-left': propsRef.value.fillWd,
+      'padding-right': propsRef.value.fillWd
+    })
+
+    const isOnlyIcon = computed(() => {
+      return propsRef.value.title === '' ? true : false
     })
 
     const hasPreIconSlot = computed(() => (slots.preIcon ? true : false))
 
     return {
       clickHandler,
+      isOnlyIcon,
+      styles,
       classes,
       isDisabled,
       hasPreIconSlot
@@ -73,7 +119,17 @@ export default defineComponent({
 @--acg-icon-bg: #777;
 @--acg-icon-bg-hover: #111;
 /* Reference to: NextUI */
+
+/* Primary */
+@--acg-bg-primary: #cee4fe;
+@--acg-bg-primaryHover: #b7d5f8;
 @--acg-colors-primary: #0072f5;
+
+/* Secondary */
+@--acg-bg-secondary: #eadcf8;
+@--acg-bg-secondaryHover: #e0cbf5;
+@--acg-colors-secondary: #7828c8;
+
 @--acg-colors-text: #11181c;
 @--acg-colors-border: rgb(0, 0, 0, 0.15);
 @--acg-color-gradient: linear-gradient(
@@ -94,7 +150,6 @@ export default defineComponent({
   padding-right: 0.875rem;
 
   height: 2.5rem;
-  line-height: 2.5rem;
 
   font-size: 0.875rem;
   font-weight: 500;
@@ -113,15 +168,19 @@ export default defineComponent({
     transform 0.25s ease 0s, opacity 0.25s ease 0s;
   position: relative;
   overflow: hidden;
-  border: none;
   cursor: pointer;
   pointer-events: auto;
   padding: 0px;
+
+  border: none;
   border-radius: 12px;
 
-  border-style: solid;
   min-width: min-content;
-  border-width: 2px;
+
+  min-width: min-content;
+  padding-left: 0.875rem;
+  padding-right: 0.875rem;
+  margin: 0.25rem;
 
   &.is-disabled {
     background: #efeef0;
@@ -132,22 +191,19 @@ export default defineComponent({
   }
 
   &.default {
-    background-color: #eceef0;
-    color: #768e8c;
+    background: #eceef0;
+    color: #7e868c;
+    &:hover {
+      background: #dfe3e6;
+    }
   }
 
   &.primary {
-    background: transparent;
-    border-color: @--acg-colors-primary;
     color: @--acg-colors-primary;
-    padding-left: 1.25rem;
-    padding-right: 1.25rem;
-    border-style: solid;
-    min-width: min-content;
-    border-width: 2px;
+    background: @--acg-bg-primary;
+
     &:hover {
-      color: #fff;
-      background: @--acg-colors-primary;
+      background: @--acg-bg-primaryHover;
     }
   }
 
@@ -167,14 +223,47 @@ export default defineComponent({
     &:hover {
       background: @--acg-color-gradient;
       color: #fff;
-      .button-prefix-icon {
-        color: #fff;
-      }
     }
 
     &.is-active {
       background: @--acg-color-gradient;
       color: #fff;
+    }
+  }
+
+  &.secondary {
+    color: @--acg-colors-secondary;
+    background: @--acg-bg-secondary;
+
+    &:hover {
+      background: @--acg-bg-secondaryHover;
+    }
+  }
+
+  &.success {
+    color: var(--acg-colors-success);
+    background: var(--acg-bg-success);
+
+    &:hover {
+      background: var(--acg-bg-successHover);
+    }
+  }
+
+  &.warning {
+    color: var(--acg-colors-warning);
+    background: var(--acg-bg-warning);
+
+    &:hover {
+      background: var(--acg-bg-warningHover);
+    }
+  }
+
+  &.error {
+    color: var(--acg-colors-error);
+    background: var(--acg-bg-error);
+
+    &:hover {
+      background: var(--acg-bg-errorHover);
     }
   }
 }
@@ -186,18 +275,23 @@ export default defineComponent({
   color: inherit;
   z-index: 100;
   padding-left: 8px;
+  &.only {
+    padding: 0px;
+  }
 }
 
-.acg-button-text {
+.acg-button__text {
   display: flex;
   align-items: center;
   justify-content: center;
   z-index: 200;
-  padding-left: 0.6rem;
-  padding-right: 0.6rem;
-  line-height: 2.5rem;
+  padding-left: 2px;
+  padding-right: 2px;
+  // line-height: 2.5rem;
 
   font-size: inherit;
+  color: inherit;
+  font-weight: inherit;
   font-family: @--acg-fonts-sans;
 }
 </style>
