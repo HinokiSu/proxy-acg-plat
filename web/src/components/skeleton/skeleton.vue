b6c17139ac200ee6a294f3873619af336ddbd3f8
HinokiSu <yukihinoki@qq.com>
2023-07-03 17:15:30 +0800

    chore: modify skeleton style and props

--- web/src/components/skeleton/skeleton.vue
+++ web/src/components/skeleton/skeleton.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="skeleton box-root">
+  <div
+    class="skeleton box-root"
+    :style="styles"
+    v-for="(_item, index) in num"
+    :key="index"
+  >
     <span class="skeleton-rectangular"> </span>
     <div class="skeleton-footer box-root">
+      <span class="skeleton-text"></span>
       <span class="skeleton-text"></span>
       <span class="skeleton-text"></span>
     </div>
@@ -9,26 +15,43 @@
 </template>
 
 <script lang="ts">
+import inlineStyles from '@hooks/inline-styles'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'Skeleton',
-  setup() {
-    return {}
+  props: {
+    w: {
+      type: String,
+      default: '400px'
+    },
+    filling: {
+      type: String,
+      default: '40px'
+    },
+    num: {
+      type: Number,
+      default: 1
+    }
+  },
+  setup(props) {
+    const styles = inlineStyles({
+      width: props.w,
+      padding: props.filling
+    })
+    return { styles }
   }
 })
 </script>
 
 <style lang="less" scoped>
 .skeleton {
-  width: 210px;
-  margin-right: 4px;
-  margin-top: 40px;
-  margin-bottom: 40px;
+  width: 400px;
+  margin: 10px 0;
 
   &-rectangular {
     display: block;
-    width: 210px;
+    width: 50%;
     height: 118px;
     background-color: rgba(0, 0, 0, 0.11);
     animation: animation-load 1.5s ease-in-out 0.5s infinite;
@@ -52,9 +75,14 @@ export default defineComponent({
     &::before {
       content: '\00a0';
     }
-
-    &:nth-last-child(1) {
-      width: 60%;
+    &:nth-child(1) {
+      width: 100%;
+    }
+    &:nth-child(2) {
+      width: 85%;
+    }
+    &:nth-child(3) {
+      width: 70%;
     }
   }
 }
