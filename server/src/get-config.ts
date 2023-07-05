d248a4a88794f521a3663981f1e42cac35af6531
HinokiSu <yukihinoki@qq.com>
2023-07-05 14:56:30 +0800

    chore: according to env get config file

--- server/src/get-config.ts
+++ server/src/get-config.ts
@@ -9,7 +9,15 @@ type TConfig = {
   ImgFilePathPrefix: string
 }
 const getServerConfig = () => {
-  const configPath = path.join(process.cwd(), path.sep, 'server.config.json')
+  const isDev = process.env.NODE_ENV === 'development'
+  let configPath = ''
+
+  if (isDev) {
+    configPath = path.join(process.cwd(), path.sep, 'server.config.dev.json')
+  } else {
+    configPath = path.join(process.cwd(), path.sep, 'server.config.json')
+  }
+  console.log(configPath)
   const config = fs.readFileSync(configPath, 'utf-8')
   if (!config) {
     console.log(`Error: Config is empty!!, config path: ${configPath}`)
