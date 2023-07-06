13f9d46ee48dae289b5e17dd8ed37406a4ea4525
HinokiSu <yukihinoki@qq.com>
2023-07-06 14:50:52 +0800

    chore: modify project version and add script

--- web/script/zip-dist.js
+++ web/script/zip-dist.js
@@ -1,16 +1,18 @@
-const fs = require('fs')
-const path = require('path')
-const archiver = require('archiver')
+import { createWriteStream } from 'fs'
+import { join, dirname } from 'path'
+import archiver from 'archiver'
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
 // zip file storage location
-const homedir = path.join(__dirname, '../build')
+const homedir = join(__dirname, '../release')
 
-console.log(homedir)
 //配置要打包的路径列表,需要打包某些目录，添加到数组里面即可 相对路径
 const target = ['dist']
 
 // 默认在当前目录路径生成此文件 dist.zip
-const output = fs.createWriteStream(homedir + '/dist.zip')
+const output = createWriteStream(homedir + '/dist.zip')
 const archive = archiver('zip', {
   zlib: { level: 9 } // 设置压缩级别
 })
@@ -22,7 +24,7 @@ archive.on('error', function (err) {
 output.on('close', function () {
   console.log(`
 --------- Archiver zip  ---------
-[Proxy-acg-plat]: Server
+[Proxy-acg-plat]: Web
 Create File Size: ${(archive.pointer() / 1024).toFixed(1)}KB
 File location: ${homedir}\\dist.zip
 --------- Zip Completed ---------
