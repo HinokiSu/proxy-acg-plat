b5f1784691b001761b37f3541cb937675943895c
HinokiSu <yukihinoki@qq.com>
2023-07-10 16:30:13 +0800

    fix: fix bug when  register user , authenticating

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { TRegisterDto, TUserDto } from '../interfaces/user.type'
 import { loginUser, registerUser } from '../services/user.services'
 import { getErrorMessage } from '../utils/responseMsgHandler'
+import { verifyToken } from '../services/auth'
 
 const userRouter = express.Router()
 
@@ -14,7 +15,7 @@ userRouter.post('/login', (req, res, next) => {
   }
 })
 
-userRouter.post('/register', (req, res, next) => {
+userRouter.post('/register',verifyToken, (req, res, next) => {
   try {
     const user = req.body as TRegisterDto
     registerUser(user)
